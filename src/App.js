bded2f8518b46b501e54bb084e964e1b97906834
Luis Troya <troyaluis56@gmail.com>
2017-08-07 19:22:34 -0400

    Add datepicker integration and fields to the form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        begin_date: '',
+        number_days: '',
+        country_code: '',
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log("Submitting the form");
+    };
+
+    update = () => {
+        this.setState({
+            number_days: this.refs.number_days.value,
+            country_code: this.refs.country_code.value,
+        });
+    };
+
+    updateDate = (date) => {
+        this.setState({
+            begin_date: date,
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <form onSubmit={this.handleSubmit}>
+                    <DatePicker
+                        dateFormat="DD/MM/YYYY"
+                        placeholder="Start date"
+                        selected={this.state.begin_date}
+                        onChange={this.updateDate}/>
+
+                    <input
+                        type="number"
+                        ref="number_days"
+                        placeholder="Number of days"
+                        onChange={this.update}/>
+
+                    <input
+                        type="text"
+                        ref="country_code"
+                        placeholder="Country code"
+                        onChange={this.update}/>
+
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
